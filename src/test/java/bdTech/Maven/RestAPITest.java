448b6e6dc035c994d5d8285032b4002d1c90a990
basith40 <basith401@gmail.com>
2023-03-06 21:41:10 -0500

    Basith 06 03 23 commit

---
+++ src/test/java/bdTech/Maven/RestAPITest.java
@@ -0,0 +1,16 @@
+package bdTech.Maven;
+
+import org.testng.annotations.Test;
+
+
+public class RestAPITest {
+	@Test
+	public void application() {
+		System.out.println("application");
+	}
+	@Test
+	public void start() {
+		System.out.println("start");
+	}
+
+}
